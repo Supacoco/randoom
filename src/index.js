ab5d20b72e96bd493ea80c28cb2ac70ef2a67e0d
Nicolas Corral <nico.corral@gmail.com>
2017-06-11 17:49:00 -0400

    fix: imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { CLib } from './generators/cLib.js'
-import { Hoole } from './generators/hoole.js'
-import { Randu } from './generators/randu.js'
-import { Windows } from './generators/windows.js'
-import { XKCD } from './generators/xkcd.js'
-import { Xorshift } from './generators/xorshift.js'
+import CLib from './generators/cLib.js'
+import Hoole from './generators/hoole.js'
+import Randu from './generators/randu.js'
+import Windows from './generators/windows.js'
+import XKCD from './generators/xkcd.js'
+import Xorshift from './generators/xorshift.js'
 
 export {
     CLib,
