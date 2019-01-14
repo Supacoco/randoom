bc72cb0e4d00f73ada53732f661aeb4df283d14b
Nicolas Corral <nicolas.corral@ubisoft.com>
2019-01-14 16:36:05 -0500

    chore: fixed/updated babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import buble from 'rollup-plugin-buble'
-import uglify from 'rollup-plugin-uglify-es'
+import buble from "rollup-plugin-buble"
+import uglify from "rollup-plugin-uglify-es"
 
 export default {
-  input: 'src/index.js',
-  plugins: [ 
+  input: "src/index.js",
+  plugins: [
     uglify(),
-    buble({ 
+    buble({
       transforms: {
         generator: false
       }
     })
   ],
   output: [
-    { file: 'dist/bundle.cjs.js', format: 'cjs' },
-    { file: 'dist/bundle.umd.js', name: 'randoom', format: 'umd' },
-    { file: 'dist/bundle.js', format: 'es' }
+    { file: "dist/bundle.cjs.js", sourcemap: true, format: "cjs"  },
+    { file: "dist/bundle.umd.js", sourcemap: true, name: "randoom", format: "umd" },
+    { file: "dist/bundle.js", sourcemap: true, format: "es" }
   ]
 }
